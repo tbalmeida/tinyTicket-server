6ce9c0e911afbd5cca284ffdda25f1c6ea564e2c
Thiago Almeida <tbalmeida@gmail.com>
2020-07-22 09:40:29 -0600

    Added event listing

--- src/routes/1.0/events.js
+++ src/routes/1.0/events.js
@@ -1,9 +1,22 @@
 const router = require("express").Router();
 
-// All queries used for cities are listed here
+// All queries used for events are listed here
+const SQL_EVENTS = "SELECT * FROM events";
 const SQL_EVENT_ADD = 'INSERT INTO events(name, description, date_time, venue, qt_tickets, max_per_user, ticket_price) VALUES (?, ?, ?, ?, ?, ?, ?)';
 
 module.exports = (pool) => {
+  router.get('/events', (req, res) => {
+    pool.query(SQL_EVENTS, function (error, results) {
+      if (error) {
+        console.log('GET/events', error.code, error.sqlMessage);
+        res.status(500).send({msg: 'Server error, try again later.', code: error.code, err_msg: error.sqlMessage})
+      } else {
+        const qFound = results.length;
+        res.status(200).json(results)
+      }
+    });
+  });
+
   router.post('/events', (req, res) => {
     pool.query(SQL_EVENT_ADD, 
       [
